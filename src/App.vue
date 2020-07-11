3c8a07afca4421d7737e334e457d6422ced7df51
Gautham Ramesh Babu <rgautham3110@gmail.com>
2020-07-12 01:52:19 +0530

    Add alternate view for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <router-view v-if="!$vuetify.breakpoint.mobile"></router-view>
+    <Error v-else></Error>
+  </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
 
+import Error from "./views/Error";
+
 export default {
   name: "App",
+
+  components: {
+    Error,
+  },
 
   methods: {
     ...mapActions(["initializeFirebase"])
---
+++ src/views/Error.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary" dark app>
+        <v-toolbar-title class="font-weight-bold">Planner</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container class="fill-height">
+        <v-row class="fill-height" align="center" justify="center">
+            <span>This is a Desktop App and is not Optimised for Mobile.</span>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
